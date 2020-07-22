1063c4a10f087be908191a99a788693805825009
Matt Mason <masonmj10@gmail.com>
2020-07-22 15:46:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -11,5 +11,11 @@ BRANDS = []
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand
+    BRANDS == BRANDS.uniq!
+  end	  
+  
 end
